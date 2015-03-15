8e04fec98a4cd29f0d5dc753128bc09157cf3e36
lxxself <lxxself@foxmail.com>
2015-03-15 16:29:25 +0800

    第一章节操作完成
    解决第一章配置问题：因为没有设置web.xml的配置

--- chapter1/src/main/java/com/smart/dao/UserDao.java
+++ chapter1/src/main/java/com/smart/dao/UserDao.java
@@ -19,7 +19,7 @@ public class UserDao {
     private JdbcTemplate jdbcTemplate;
 
     public int getMatchCount(String userName,String password){
-        String sqlStr = "SELECT count(*) FROM t_user "+ " WHERE username=? and password=?";
+        String sqlStr = "SELECT count(*) FROM t_user "+ " WHERE user_name=? and password=?";
         return jdbcTemplate.queryForInt(sqlStr,new Object[]{userName,password} );
     }
 
